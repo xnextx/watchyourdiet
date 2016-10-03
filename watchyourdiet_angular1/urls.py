00de29a49346b535748c63d819fce2d7ddbca932
xnextx <piotr@placenet.pl>
2016-10-03 21:48:20 +0200

    -added the first view (working templates)

--- watchyourdiet_angular1/urls.py
+++ watchyourdiet_angular1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app_watchyourdiet_angular1 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.main_site, name='Main site'),
+
 ]
