764ae1d45344edd901beaf7bde0140d1fe2301b4
xnextx <piotr@placenet.pl>
2016-10-14 16:40:18 +0200

    -added tests JS, karma + Jasmine

--- static/js/logic-compiled.js
+++ static/js/logic-compiled.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var Application = angular.module('Application', []).config(function ($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken'; //Elements for Django
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken'
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
 
 Application.controller('main_site', ['$scope', '$http', 'Dev', function ($scope, $http, Dev) {
@@ -20,7 +22,7 @@ Application.controller('main_site', ['$scope', '$http', 'Dev', function ($scope,
             $scope.products.push({
                 "name": "",
                 "size": 0
-            })
+            });
         }
     };
 
@@ -28,28 +30,23 @@ Application.controller('main_site', ['$scope', '$http', 'Dev', function ($scope,
         if (check_many_products() > 1) {
             $scope.products.pop();
         }
-
     };
 
-
     //
-//*****************************************
-//
-//    Developer Version
-//
-//*****************************************
+    //*****************************************
+    //
+    //    Developer Version
+    //
+    //*****************************************
 
     Dev.SendTestData();
     // Dev.DeleteTestData(20);
-
 }]);
-
 
 Application.factory('Dev', ['$http', function ($http) {
 
-
     return {
-        SendTestData: function () {
+        SendTestData: function SendTestData() {
             $http.post("/api/v1/MyMeal/", {
                 "owner": 1,
                 "product": [], //TODO: Dlaczego dając [] wysyła się poprawnie, a cokolwiek innego już nie? nie chce tak!
@@ -57,8 +54,10 @@ Application.factory('Dev', ['$http', function ($http) {
 
             });
         },
-        DeleteTestData: function (pk) {
-            $http.delete(`/api/v1/MyMeal/${pk}`)
+        DeleteTestData: function DeleteTestData(pk) {
+            $http.delete('/api/v1/MyMeal/' + pk);
         }
     };
 }]);
+
+//# sourceMappingURL=logic-compiled.js.map
---
+++ static/js/tests/add_new_product.test.js
@@ -0,0 +1,5 @@
+describe('Users factory', function () {
+    it('has a dummy spec to test 2 + 2', function () {
+        expect(2 + 3).toBe(5);
+    });
+});
